23b440b1cc7d2fe3b3d00c50b8bd8905ccd064e2
Sidney Outeiro <sid@dcc.ufrj.br>
2021-12-07 15:53:30 -0300

    preparando para o review

--- dump.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE public.recommendations (
-	"id" serial NOT NULL,
-	"name" varchar(255) NOT NULL,
-	"link" varchar(255) NOT NULL UNIQUE,
-	"score" integer NOT NULL DEFAULT '0',
-	CONSTRAINT "recommendations_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
-);
